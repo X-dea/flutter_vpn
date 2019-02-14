9c89cee27b3145d72f0f48bef0d3e0a8a6224972
Jerry <realflyingblu@gmail.com>
2019-02-14 17:15:19 +0800

    Initial commit of iOS support

--- ios/Classes/SwiftFlutterVpnPlugin.swift
+++ ios/Classes/SwiftFlutterVpnPlugin.swift
@@ -6,6 +6,13 @@ public class SwiftFlutterVpnPlugin: NSObject, FlutterPlugin {
     let channel = FlutterMethodChannel(name: "flutter_vpn", binaryMessenger: registrar.messenger())
     let instance = SwiftFlutterVpnPlugin()
     registrar.addMethodCallDelegate(instance, channel: channel)
+    channel.setMethodCallHandler({
+            (call: FlutterMethodCall, result: FlutterResult) -> Void in
+            if ("connect" == call.method) {
+                connectVPN(result: result, usrname: call.arguments['username'], pwd: call.arguments['password'], add: call.arguments['address'])
+            } else if ("disconnect" == call.method) {
+                stopVPN(result: result)
+            }
   }
 
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
