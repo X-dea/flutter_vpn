ecebe3b2f24ad0f19b48cf8446a2a7c24c08a5a5
Jason C.H <ctrysbita@outlook.com>
2019-09-02 10:13:28 +0800

    Change LICENSE to LGPL

--- ios/Classes/VPNHandler.swift
+++ ios/Classes/VPNHandler.swift
@@ -1,15 +1,15 @@
 /**
  * Copyright (C) 2019 Jerry Wang, Jason C.H
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  */
 
 import Foundation
--- lib/flutter_vpn.dart
+++ lib/flutter_vpn.dart
@@ -1,15 +1,15 @@
 /**
  * Copyright (C) 2018 Jason C.H
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
  *
- * This program is distributed in the hope that it will be useful,
+ * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
  */
 
 import 'dart:io';
@@ -80,8 +80,8 @@ class FlutterVpn {
   ///
   /// Use given credentials to connect VPN (ikev2-eap).
   /// This will create a background VPN service.
-  static Future<Null> simpleConnect(
-      String address, String username, String password) async {
+  static Future<Null> simpleConnect(String address, String username,
+      String password) async {
     await _channel.invokeMethod('connect',
         {'address': address, 'username': username, 'password': password});
   }
