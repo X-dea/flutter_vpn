d244e0e92ce8d7cbbfc68d287c19937a737c0642
Kamran Pirwani <kamranp619@gmail.com>
2020-05-15 21:15:47 -0500

    unregister to fix duplicate calls firing

--- android/src/main/kotlin/io/xdea/flutter_vpn/FlutterVpnPlugin.kt
+++ android/src/main/kotlin/io/xdea/flutter_vpn/FlutterVpnPlugin.kt
@@ -32,6 +32,7 @@ import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
 import org.strongswan.android.logic.VpnStateService
+import io.flutter.plugin.common.PluginRegistry
 
 /** FlutterVpnPlugin */
 class FlutterVpnPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
@@ -106,13 +107,17 @@ class FlutterVpnPlugin : FlutterPlugin, MethodCallHandler, ActivityAware {
       "prepare" -> {
         val intent = VpnService.prepare(activityBinding.activity.applicationContext)
         if (intent != null) {
-          activityBinding.addActivityResultListener { req, res, _ ->
-            if (req == 0 && res == RESULT_OK)
+          var listener: PluginRegistry.ActivityResultListener? = null
+          listener = PluginRegistry.ActivityResultListener { req, res, _ ->
+            if (req == 0 && res == RESULT_OK) {
               result.success(true)
-            else
+            } else {
               result.success(false)
+            }
+            listener?.let { activityBinding.removeActivityResultListener(it) };
             true
           }
+          activityBinding.addActivityResultListener(listener)
           activityBinding.activity.startActivityForResult(intent, 0)
         } else {
           // If intent is null, already prepared
