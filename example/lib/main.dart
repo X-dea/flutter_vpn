0b06ad374bb7e152edbb52ba572bdc9b1e1de52e
Maximilian Schelbach <maximilian@schelbach.de>
2021-01-04 09:13:23 +0100

    Fix FlutterVpn.onStateChanged in iOS (#65)
    * fix FlutterVpn.onStateChanged on iOS
    * fix doc comments

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -1,16 +1,16 @@
-/**
- * Copyright (C) 2018 Jason C.H
- *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- */
+///
+/// Copyright (C) 2018 Jason C.H
+///
+/// This library is free software; you can redistribute it and/or
+/// modify it under the terms of the GNU Lesser General Public
+/// License as published by the Free Software Foundation; either
+/// version 2.1 of the License, or (at your option) any later version.
+///
+/// This library is distributed in the hope that it will be useful,
+/// but WITHOUT ANY WARRANTY; without even the implied warranty of
+/// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+/// Lesser General Public License for more details.
+///
 
 import 'package:flutter/material.dart';
 import 'package:flutter_vpn/flutter_vpn.dart';
